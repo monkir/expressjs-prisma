4706abe4555e364e37cbcc0004307c10fc55a7b2
monkir <monkirchowdhury@gmail.com>
2025-03-27 21:41:45 +0600

    vercel configuration

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const app = require('./server');
-const serverless = require('serverless-http');
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./src/routes/userRoutes');
+const postRoutes = require('./src/routes/postRoutes');
 
-module.exports = serverless(app);
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Vercel!');
+});
+
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
